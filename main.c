360de437eaa21b0a598cb76139e3530f0d1a495a
amirerfantim <amirerfantim@gmail.com>
2021-02-03 19:27:58 +0330

    feat: add func
    added print_map func to show raw map of the game.

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
-
+void print_map();
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int row, column;
+    scanf("%d %d", &row, &column);
+    print_map(row,column);
+}
+
+void print_map(int row, int column){
+    int i1, i2, i3, col_counter = 1;
+    printf("   ");
+
+    for(i3 = 0; i3 < 4*column; i3++) {
+        if((i3 +2) % 4 == 0) {
+            printf("%d", col_counter);
+            col_counter++;
+        }else{
+            printf("-");
+        }
+    }
+
+    printf("-\n");
+
+    for(i1 = 0; i1 < row; i1++){
+        printf("%3d", i1+1);
+        for(i2 = 0; i2 < column; i2++){
+            printf("|   ");
+        }
+
+        printf("|\n   ");
+
+        for(i3 = 0; i3 < 4*column; i3++) {
+            printf("-");
+        }
+        printf("-\n");
+    }
+
 }
